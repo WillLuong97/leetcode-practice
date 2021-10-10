5752d184843eb9ebc382a198aea0f7d54cf8e749
Will Luong <tluong@stedwards.edu>
2021-10-09 19:24:47 -0500

    added junitt4 to the project

---
+++ src/com/javaprojects/Test/DynamicProgrammingTest/FibonacciNumberTest.java
@@ -0,0 +1,10 @@
+package com.javaprojects.Test.DynamicProgrammingTest;
+
+import org.junit.Test;
+
+public class FibonacciNumberTest {
+    @Test
+    public void testFib(){
+        //write some test cases here later!!!
+    }
+}
